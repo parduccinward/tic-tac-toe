53666411b03f34f440ab54472061da90c7d46468
Diego <dpardoblacutt@gmail.com>
2022-09-29 18:19:39 -0400

    Create pseudocode to code along

---
+++ script.js
@@ -0,0 +1,15 @@
+// Use a module to objects that you need only once.
+// And factories for objects that you need more than once
+
+// 1. Gameboard object
+// 1.1. Function to render array to webpage (check)
+// 2. Player object
+// 2.1. Function to add a mark to a specific spot (check)
+// 3. Game object
+// 3.1. Function that checks when game is over (3-row or tie) (check)
+// 3.2. Function to restart game (check)
+// 3.2.1. Create new player 1 and 2 (with names)
+
+// Decide how and what happens when game starts
+// Decide how and what happens when game ends
+// Decide how and what happens when game restart
